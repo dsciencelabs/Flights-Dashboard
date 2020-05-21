cea9e6ffaeff32b539620603a753c2e7244446e1
Bakti Siregar <13848017+Bakti-Siregar@users.noreply.github.com>
2020-05-21 15:26:13 +0700

    Update ui.R

--- Database-External/ui.R
+++ Database-External/ui.R
@@ -24,7 +24,7 @@ ui <- dashboardPage(title = "www.baktisiregar.com",
           selected = 99,
           size = 13,
           selectize = FALSE),
-        actionLink("remove", "Remove detail tabs")),
+        actionLink("remove", icon = icon("sync-alt"),"Remove detail tabs"),
         menuItem("About Me", icon = icon("linkedin"), href = "https://www.linkedin.com/in/bakti-siregar-15955480/"),
         menuItem("Source Code", icon = icon("github"), href = "https://github.com/Bakti-Siregar/Flights-Dashboard"),
         menuItem("Video", icon = icon("youtube"), href = "https://www.youtube.com/watch?v=vZR_2hhvsu4")
